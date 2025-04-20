8dee502ac115b96735428a0ef33564fbb5a3077f
AnshulVermaQ <vermaanshul975@gmail.com>
2025-04-20 07:55:19 +0530

    connected to database

---
+++ ChatApp/backend/controller/auth.controller.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+export const signup = async(req,res) =>{
+
+}
+
+export const login = async(req,res) =>{
+    
+}
+
+export const logout = async(req,res) =>{
+    
+}
--- ChatApp/backend/index.js
+++ ChatApp/backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import {connectDB} from './database/db.js';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+dotenv.config();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
